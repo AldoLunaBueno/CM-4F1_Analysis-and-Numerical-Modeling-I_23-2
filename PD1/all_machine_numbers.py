a4b4333f396d3eb687943f7036f5027f6f1dd53b
AldoLunaBueno <alunab@uni.pe>
2023-09-13 11:27:39 -0500

    Done output

--- PD1/all_machine_numbers.py
+++ PD1/all_machine_numbers.py
@@ -1,9 +1,11 @@
-def print_all_machine_numbers_base2(t, L, U):
+def print_all_machine_numbers_base2(beta, t, L, U):
     """
     t: dígitos significativos
     L: mínimo exponente
     U: máximo exponente
     """
+
+    beta = 2 # No está implementado en otras bases
 
     for i in range(L, U+1):
         for j in range(2**(t-1)):
@@ -15,5 +17,18 @@ def print_all_machine_numbers_base2(t, L, U):
 def total_machine_numbers(beta, t, L, U):
     return 2 * (beta - 1) * beta**(t-1) * (U - L + 1)
 
-print_all_machine_numbers_base2(3, -2, 2)
-# print(total_machine_numbers(2, 3, -2, 2))
+beta = 2
+t = 3
+L = -2
+U = 2
+print("Conjunhto de números máquina:")
+print(f"F({beta}, {t}, {L}, {U})")
+print()
+
+print("Total de números máquina: ")
+print(total_machine_numbers(beta, t, L, U))
+print()
+
+print("Todos los números máquina: ")
+print_all_machine_numbers_base2(beta, t, L, U)
+print()
